a9978a658afa316d2fde321f3daebea0b4b40d0c
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-12-24 01:41:22 +0100

    Add useState to taskform

--- src/app/components/TaskForm.js
+++ src/app/components/TaskForm.js
@@ -1,8 +1,46 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { v4 as uuid } from "uuid";
+import { addTask } from "../features/tasks/taskSlice";
+
 function TaskForm() {
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      addTask({
+        ...task,
+        id: uuid(),
+      })
+    );
+  };
+
   return (
-    <form action="">
-      <input type="text" placeholder="title" />
-      <textarea name="description" placeholder="description"></textarea>
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        placeholder="description"
+        onChange={handleChange}
+      ></textarea>
       <button>Save</button>
     </form>
   );
