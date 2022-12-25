3e10ad024c7bdf9fcb38bebfee96b280991af807
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-12-26 00:48:19 +0100

    Add handleDelete function

--- src/app/components/TasksList.js
+++ src/app/components/TasksList.js
@@ -2,12 +2,18 @@ import { useSelector } from "react-redux";
 
 function TasksList() {
   const tasks = useSelector((state) => state.tasks);
+
+  const handleDelete = (id) => {
+    console.log(id);
+  };
+
   return (
     <div>
       {tasks?.map((task) => (
         <div key={task.id}>
           <h3>{task.title}</h3>
           <p>{task.description}</p>
+          <button onClick={() => handleDelete(task.id)}>Delete</button>
         </div>
       ))}
     </div>
