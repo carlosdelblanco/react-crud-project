69be1a390601dd4cd10af8c604048ca07ba526bd
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-12-26 23:23:59 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+
 import TaskForm from "./app/components/TaskForm";
 import TasksList from "./app/components/TasksList";
 
@@ -6,8 +8,12 @@ function App() {
   return (
     <div className="App">
       <h1>TaskList project using React</h1>
-      <TaskForm />
-      <TasksList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TasksList />} />
+          <Route path="/create-task" element={<TaskForm />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
